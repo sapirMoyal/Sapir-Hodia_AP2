ab8fc40eacfa2bf66d636414492c7c498856f3a8
hodaya <hodaya@HODAYA-HP>
2017-04-05 15:48:34 +0300

    Add project files.

---
+++ ConsoleApplication3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MazeGeneratorLib;
+using MazeLib;
+using SearchAlgorithmsLib;
+namespace ConsoleApplication3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IMazeGenerator img = new DFSMazeGenerator();
+            Maze maze = img.Generate(100, 100);
+            
+            Console.WriteLine(maze.ToString());
+            ISearcher searcher = new BFS(); 
+            searcher.search()
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ConsoleApplication3/SearchableMaze.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SearchAlgorithmsLib;
+using MazeGeneratorLib;
+using MazeLib;
+namespace ConsoleApplication3
+{
+    class SearchableMaze : ISearchable
+    {
+        private Maze maze;
+        public List<State<Position>> getAllPossibleStates(State<> s)
+        {
+            
+        }
+
+        public State<Position> getGoalState()
+        {
+             State<Position> s  = new State<Position> (maze.GoalPos);
+            return s;
+        }
+
+        public State<Position> getInitialState()
+        {
+            State<Position> s = new State<Position> (maze.InitialPos);
+            return s;
+        }
+    }
+}
