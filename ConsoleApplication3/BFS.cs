928917853d73ba5f48e522a26ec30f4aca60fa07
hodaya <hodaya@HODAYA-HP>
2017-04-05 15:52:29 +0300

    first commit

---
+++ ConsoleApplication3/BFS.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SearchAlgorithmsLib
+{
+    public class BFS : ISearcher
+    {
+        public int getNumberOfNodesEvaluated()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Solution search(ISearchable searchable)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
